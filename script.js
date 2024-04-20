106e6dfe97aef18a583c6f559d7e2f06e5901689
mikocchi2 <161132482+mikocchi2@users.noreply.github.com>
2024-04-20 17:29:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Fetch the match history data from your API
-    fetch('https://243c-79-101-36-217.ngrok-free.app/games')
+    fetch('https://243c-79-101-36-217.ngrok-free.app/api/games')
     .then(response => response.json())  // Convert the returned response into JSON
     .then(data => {
         // Handle the data
