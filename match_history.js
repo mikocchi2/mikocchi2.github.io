abd83840a8010e69258a4c6f238a76be0bc455b7
mikocchi2 <161132482+mikocchi2@users.noreply.github.com>
2024-04-28 05:39:11 +0200

    Update match_history.js

--- match_history.js
+++ match_history.js
@@ -42,6 +42,17 @@ document.addEventListener('DOMContentLoaded', () => {
             csDiffDiv.textContent = `CS difference: ${match.cs_difference}`;
             matchDiv.appendChild(csDiffDiv);
 
+            const acScoreDiv = document.createElement('div');
+            acScoreDiv.className = 'match-history-stat'
+            acScoreDiv.textContent = `AC score: ${match.ac_score}`;
+            matchDiv.appendChild(acScoreDiv);
+
+            const laneDiffDiv = document.createElement('div');
+            laneDiffDiv.className = 'match-history-stat';
+            //laneDiffDiv.style.backgroundColor = game_color(match);
+            laneDiffDiv.textContent = bad_or_good(match);
+            matchDiv.appendChild(laneDiffDiv);
+
             // Append the complete match div to the container
             matchHistoryContainer.appendChild(matchDiv);
         });
@@ -63,10 +74,66 @@ function game_color(match){
         case 4:     color ='#B31F00'; break;
         case 5:     color ='#992900'; break;
         case 6:     color ='#803400'; break;
+        case 7:     color ='#4D4A00'; break;
+        case 8:     color ='#335400'; break;
+        case 9:     color ='#1A6900'; break;
+        case 10:    color ='#1A6900'; break;
+        default:    color ='000000'; break;
+    }
+    return color
+}
+
+function lane_diff_color(match){
+    let color = "";
+    if (acScore_difference <= 0){
+        color = "#e60000"
+    }
+    else {
+        color = "#2D79FF"
+    }
+}
+
+function bad_or_good(match){
+    if (match.ac_score_difference <= 0){
+        return "LOSER"
+    }
+    else if (match.ac_score_difference > 0 && match.ac_score_difference <= 3){
+        return "EVEN"
+    }
+    else if (match.ac_score_difference > 3){
+        return "GG"
+    }
+    else return "BAG"
+}
+
+/* chat gpt
+ case 1:     color ='#FF0000'; break;
+        case 1:     color ='#FF0000'; break;
+        case 2:     color ='#E60A00'; break;
+        case 3:     color ='#CC1400'; break;
+        case 4:     color ='#B31F00'; break;
+        case 5:     color ='#992900'; break;
+        case 6:     color ='#803400'; break;
         case 7:     color ='#663F00'; break;
         case 8:     color ='#4D4A00'; break;
         case 9:     color ='#335400'; break;
         case 10:    color ='#1A6900'; break;
     }
-    return color
-}
+
+*/
+
+/* moje
+    case 1:     color ='#990000	 '; break;
+    case 2:     color ='#cc0000 '; break;
+    case 3:     color ='#ff0000 '; break;
+    case 4:     color ='#ff3333'; break;
+    case 5:     color ='#e6b800 '; break;
+    case 6:     color ='#ffcc00 '; break;
+    case 7:     color ='#ffd11a'; break;
+    case 8:     color ='#00cc00'; break;
+    case 9:     color ='#00b300'; break;
+    case 10:    color ='#009900'; break;
+
+
+
+*/ 
