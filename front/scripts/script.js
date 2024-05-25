3f59bbe92632d60410920e40bce903f8c303f105
mikocchi2 <161132482+mikocchi2@users.noreply.github.com>
2024-05-25 07:48:27 +0200

    Update script.js

--- front/scripts/script.js
+++ front/scripts/script.js
@@ -37,10 +37,11 @@ function splitStringByHash(inputString) {
 }
 
 function viewResults() {
-    window.location.href = '/matchHistory.html';
+    window.location.href = 'https://mikocchi2.github.io/front/matchHistory.html';
 }
 
 function viewDistribution() {
-    window.location.href = '/distribution.html';
+    window.location.href = 'https://mikocchi2.github.io/front/distribution.html';
 }
+
 
